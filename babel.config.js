c427f67646a53c5f484c172fe1dc726f797bf192
aghthar <theaghthar@gmail.com>
2024-12-11 20:10:44 +0200

    Adds Jest and React Native testing libraries for improved testing capabilities. Updates Babel configuration to include React Native Reanimated plugin. Removes unnecessary Babel presets and plugins. Updates package.json to include new testing scripts and dependencies.

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
-    ],
-    plugins: [
-      [
-        "module-resolver",
-        {
-          root: ["./src"],
-          alias: {
-            "@": "./src",
-          },
-        },
-      ],
-    ],
+    presets: ["babel-preset-expo"],
+    plugins: ["react-native-reanimated/plugin"],
   };
 };
