7d0abba3ab1081f13979373701ce0688489a5d0a
aghthar <theaghthar@gmail.com>
2024-12-11 20:24:54 +0200

    Updates Babel configuration for nativewind integration
    Updates Babel configuration to include nativewind presets and module-resolver plugin for improved module resolution and JSX import source.

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,20 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: ["react-native-reanimated/plugin"],
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@": "./src",
+          },
+        },
+      ],
+    ],
   };
 };
